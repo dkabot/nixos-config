918bf775bea7c283bab8dafb028b09ecb2173f34
dkabot <1316469+dkabot@users.noreply.github.com>
2019-08-24 13:05:51 -0400

    comment home config, add firefox touch scrolling and file sorting

--- homecfg/common/naomi/home.nix
+++ homecfg/common/naomi/home.nix
@@ -10,20 +10,24 @@ in
 
 {
   imports = [
+    # Import home-manager itself.
     "${home-manager}/nixos"
     # Dynamically importing per-machine home configs would be cool but looks infeasible.
     #(../../machines + "/${config.networking.hostName}" + /naomi/home.nix)
   ];
 
   home-manager.users.naomi = {
+    # User-scope packages; not neeeded system-wide.
     home.packages = with pkgs; [
       discord slack bitwarden
       meld filezilla
       neofetch
     ];
 
+    # Firefox could be added as a package above, but this allows for expansion if desired.
     programs.firefox.enable = true;
 
+    # Basic git configuration.
     programs.git = {
       enable = true;
       userName = "dkabot";
@@ -32,19 +36,32 @@ in
 
     home.sessionVariables = {
       EDITOR = "nano";
+      MOZ_USE_XINPUT2 = 1;
     };
 
+    # Apply Adwaita-dark theme, because it is nice.
     gtk.enable = true;
     gtk.theme.name = "Adwaita-dark";
 
+    # Various GNOME settings.
     dconf.enable = true;
     dconf.settings = {
+      # Set the favorites list in the Dash.
       "org/gnome/shell" = {
          favorite-apps = ["firefox.desktop" "org.gnome.Nautilus.desktop" "org.gnome.Terminal.desktop"];
       };
+      # Prevent automatic sleep while on AC power.
       "apps/gnome-power-manager/timeout" = {
          sleep_computer_ac = 0;
       };
+      # Sort folders before files in Nautilus and the file chooser.
+      "org/gnome/nautilus/preferences" = {
+         sort-directories-first = true;
+      };
+      "org/gtk/settings/file-chooser" = {
+         sort-directories-first = true;
+      };
+
     };
 
     xdg.enable = true;
