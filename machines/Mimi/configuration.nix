328e939c284d39073c883d2064f7c60f2e21fb49
dkabot <1316469+dkabot@users.noreply.github.com>
2019-08-25 21:53:03 -0400

    add Mimi

---
+++ machines/Mimi/configuration.nix
@@ -0,0 +1,39 @@
+# NixOS system-specific configuration file for Mimi.
+
+{ config, pkgs, ... }:
+
+{
+  imports = [
+      # Import the results of the hardware scan and apply common configuration.
+      ./hardware-configuration.nix
+      ../../config
+      # Apply HiDPI console.
+      ../../config/hidpi-console.nix
+      # Apply Surface configs without Jakeday kernel.
+      ../../config/surface-config.nix
+      # Log naomi in automatically.
+      (import ../../config/gdm-autologin.nix { inherit config; username = "naomi"; })
+  ];
+
+  # Enable Surface firmware.
+  hardware.firmware = with pkgs; [
+    surface-firmware-go
+  ];
+
+  # LUKS configuration.
+  boot.initrd.luks.devices = [
+    {
+      name = "Mimi-crypt";
+      device = "/dev/disk/by-uuid/8f8dfe77-fc6a-4283-a4e9-1bef81c98b19";
+      preLVM = true;
+      allowDiscards = true;
+    }
+  ];
+
+  # Set the hostname.
+  networking.hostName = "Mimi";
+
+  # The danger setting; see 'default-configuration.nix' for details.
+  system.stateVersion = "19.09";
+
+}
