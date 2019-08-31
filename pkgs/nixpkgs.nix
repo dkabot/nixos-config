dbb29f0a23fb7ac5e5a17f9799e3fbce6eaff61b
dkabot <1316469+dkabot@users.noreply.github.com>
2019-08-31 17:28:28 -0400

    add chicago95 theme package, though unused

--- pkgs/nixpkgs.nix
+++ pkgs/nixpkgs.nix
@@ -8,6 +8,7 @@
     config.allowUnfree = true;
 
     overlays = [ (self: super: {
+        chicago95-theme = super.callPackage ./chicago95 { };
         linux-surface-control = super.callPackage ./linux-surface-control { };
         surface-firmware-book-2-15 = super.callPackage ./surface-firmware { surfaceModel = "Book-2-15"; };
         surface-firmware-go = super.callPackage ./surface-firmware { surfaceModel = "Go"; };
