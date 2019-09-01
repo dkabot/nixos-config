7d0ff82f68a86bd17d3e93414fc2299d3ae4f0f7
dkabot <1316469+dkabot@users.noreply.github.com>
2019-09-01 19:16:57 -0400

    shorten the names of sweet and chicago95, add arc-menu and dash-to-panel with matching configs

--- pkgs/nixpkgs.nix
+++ pkgs/nixpkgs.nix
@@ -8,15 +8,30 @@
     config.allowUnfree = true;
 
     overlays = [ (self: super: {
-        # Theming
-        chicago95-theme = super.callPackage ./chicago95 { };
-        sweet-gtk-theme = super.callPackage ./sweet { };
-        candy-icons = super.callPackage ./candy-icons { };
-        sweet-folders = super.callPackage ./sweet-folders { };
+        # GNOME Extensions
+        gnomeExtensions = super.recurseIntoAttrs {
+          arc-menu = super.callPackage ./arc-menu { };
+          dash-to-panel = super.gnomeExtensions.dash-to-panel.overrideAttrs ( old: rec {
+            name = "${pname}-${version}"; # Need to manually specify, or it will use the prior -19 name.
+            pname = "gnome-shell-dash-to-panel";
+            version = "22";
+            src = super.fetchFromGitHub {
+              owner = "home-sweet-gnome";
+              repo = "dash-to-panel";
+              rev = "v${version}";
+              sha256 = "05v17887s44p6y17lf8aa2hfak84x1qifcq9d9c6y59mkyjs6g7n";
+            };
+          });
+        };
         # Surface-related packages
         linux-surface-control = super.callPackage ./linux-surface-control { };
         surface-firmware-book-2-15 = super.callPackage ./surface-firmware { surfaceModel = "Book-2-15"; };
         surface-firmware-go = super.callPackage ./surface-firmware { surfaceModel = "Go"; };
+        # Theming
+        chicago95 = super.callPackage ./chicago95 { };
+        sweet = super.callPackage ./sweet { };
+        candy-icons = super.callPackage ./candy-icons { };
+        sweet-folders = super.callPackage ./sweet-folders { };
       }
     ) ];
 
--- users/naomi/gnome.nix
+++ users/naomi/gnome.nix
@@ -6,21 +6,25 @@
     # Apply Sweet-Dark theme and accompanying icon theme.
     gtk.enable = true;
     gtk.theme.name = "Sweet-Dark";
-    gtk.theme.package = pkgs.sweet-gtk-theme;
+    gtk.theme.package = pkgs.sweet;
     gtk.iconTheme.name = "Sweet-Purple";
     gtk.iconTheme.package = pkgs.sweet-folders;
-    # Additional icon themes relied upon by Sweet-Purple.
-    home.packages = with pkgs; [
-      candy-icons zafiro-icons
-    ];
 
+    home.packages = with pkgs; [
+      # Additional icon themes relied upon by Sweet-Purple.
+      candy-icons zafiro-icons
+      # Extensions.
+      gnomeExtensions.dash-to-panel gnomeExtensions.arc-menu
+    ];
 
     # Various GNOME settings.
     dconf.enable = true;
     dconf.settings = {
-      # Set the favorites list in the Dash.
       "org/gnome/shell" = {
-        favorite-apps = [ "firefox.desktop" "org.gnome.Nautilus.desktop" "org.gnome.Terminal.desktop" ];
+        # Set the favorites list in the Dash (or Panel).
+        favorite-apps = [ "firefox.desktop" "org.gnome.Nautilus.desktop" ];
+        # Enable extensions.
+        enabled-extensions = [ "user-theme@gnome-shell-extensions.gcampax.github.com" "dash-to-panel@jderose9.github.com" "arc-menu@linxgem33.com" ];
       };
       # Prevent automatic sleep while on AC power.
       "org/gnome/settings-daemon/plugins/power" = {
@@ -39,6 +43,39 @@
       };
       "org/gnome/desktop/screensaver" = {
         picture-uri = "file:///home/naomi/.config/background.png";
+      };
+      # Enable Minimize and Maximize buttons.
+      "org/gnome/desktop/wm/preferences" = {
+        button-layout = "appmenu:minimize,maximize,close";
+      };
+      # Apply shell theme.
+      "org/gnome/shell/extensions/user-theme" = {
+        name = "Sweet-Dark";
+      };
+      # Dash to Panel settings.
+      "org/gnome/shell/extensions/dash-to-panel" = {
+        panel-size = 40; # Panel height.
+        location-clock = "STATUSRIGHT"; # Clock location, all the way to the right (though Show Desktop is further right).
+        appicon-margin = 7; # Distance between favorite/task icons.
+        click-action = "TOGGLE-SHOWPREVIEW"; # On task click, toggle minimize/shown if single window, or show previews if multiple.
+        show-window-previews-timeout = 500; # Wait 500ms to show previews on hover.
+        leave-timeout = 500; # Wait 500ms to stop showing previews on hover exit.
+        window-preview-animation-time = 150; # 150ms animation time when showing or hiding window previews.
+        trans-use-custom-bg = true; # Enable custom panel background color (versus inheriting from shell theme).
+        trans-bg-color = "#221a3f"; # Custom panel background color value.
+        trans-use-custom-opacity = true; # Enable custom panel transparency (versus inheriting from shell theme).
+        trans-panel-opacity = 0.65; # 65% panel transparency.
+        isolate-monitors = true; # Keep tasks on different monitors separate.
+        scroll-panel-action = "NOTHING"; # Don't switch workspaces on panel scroll.
+        scroll-icon-action = "NOTHING"; # Don't change application windows on icon scroll.
+        hot-keys = true; # Enable Super + <num> hotkeys.
+        show-show-apps-button = false; # Hide Applications button, for Arc-Menu
+      };
+      # Arc Menu settings.
+      "org/gnome/shell/extensions/arc-menu" = {
+        enable-pinned-apps = false; # Show full application menu instead of pins.
+        menu-hotkey = "SUPER_L"; # Open with left super key.
+        multi-monitor = true; # Enable multi monitor support.
       };
     };
 
