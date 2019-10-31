c737258a0bfdb5cd8fa7924c787ee783a0e68ad8
dkabot <1316469+dkabot@users.noreply.github.com>
2019-10-31 18:46:08 -0400

    Enable tap to click on Kagamiin.

--- users/naomi/machines/Kagamiin/home.nix
+++ users/naomi/machines/Kagamiin/home.nix
@@ -8,12 +8,16 @@
     ../../gnome.nix
   ];
 
-  # Double arc-menu height to hackily account for hidpi. This would break horribly on external monitors, though...
   home-manager.users.naomi = {
     dconf.enable = true;
     dconf.settings = {
+      # Double arc-menu height to hackily account for hidpi. This would break horribly on external monitors, though...
       "org/gnome/shell/extensions/arc-menu" = {
         menu-height = 1100;
+      };
+      # Enable tap to click, as this touchpad really needs it.
+      "org/gnome/desktop/peripherals/touchpad" = {
+        tap-to-click = true;
       };
     };
   };
