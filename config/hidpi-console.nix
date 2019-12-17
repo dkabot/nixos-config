e351a43871788985b841673a546d0ebbb8eb7ead
dkabot <1316469+dkabot@users.noreply.github.com>
2019-12-16 21:06:26 -0500

    Update HiDPI console settings to match new names.

--- config/hidpi-console.nix
+++ config/hidpi-console.nix
@@ -4,8 +4,8 @@
 
 {
   # Font is ter-132b
-  i18n.consoleFont = "${pkgs.terminus_font}/share/consolefonts/ter-132b.psf.gz";
+  console.font = "${pkgs.terminus_font}/share/consolefonts/ter-132b.psf.gz";
 
   # Apply font as soon as possible, for LUKS etc.
-  boot.earlyVconsoleSetup = true;
+  console.earlySetup = true;
 }
