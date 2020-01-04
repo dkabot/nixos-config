f5d7a69b6693ad3214b265587eeaa4a64c354bbd
dkabot <1316469+dkabot@users.noreply.github.com>
2020-01-04 09:11:26 -0500

    Add plasma-browser-integration and NetworkManager for Plasma.

--- config/plasma.nix
+++ config/plasma.nix
@@ -12,8 +12,13 @@
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
+  # Enable NetworkManager (not automatic for Plasma).
+  networking.networkmanager.enable = true;
+
   # Add additional applications.
   environment.systemPackages = with pkgs; [
+    # System software not installed by default.
+    plasma-browser-integration
     # Basic tools not installed by default.
     vlc notepadqq spectacle okular gwenview ark
   ];
