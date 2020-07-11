7cf14896e3b4daac5e2c09b294eb1afb849ae9ec
dkabot <1316469+dkabot@users.noreply.github.com>
2020-07-11 17:06:58 -0400

    Konata has been reinstalled.

--- machines/Konata/configuration.nix
+++ machines/Konata/configuration.nix
@@ -8,21 +8,16 @@
       ./hardware-configuration.nix
       ../../config/gnome.nix # GNOME Desktop.
       ../../config/systemd-boot.nix # systemd-boot bootloader.
-      # Use GRUB bootloader.
-      (import ../../config/grub.nix { inherit config; grubDevice = "/dev/disk/by-id/ata-SanDisk_Ultra_II_960GB_161401805476"; encryptedDisk = false; })
       # Log naomi in automatically.
-      (import ../../config/autologin.nix { inherit config; displayManager = "gdm"; username = "naomi"; })
+      (import ../../config/autologin.nix { inherit config; username = "naomi"; })
       # Machine-specific home configuration for naomi.
       ../../users/naomi/machines/Konata/home.nix
   ];
 
-  # Enable nVidia proprietary drivers.
-  services.xserver.videoDrivers = [ "nvidia" ];
-
   # LUKS configuration.
   boot.initrd.luks.devices = {
     Konata-crypt = {
-      device = "/dev/disk/by-uuid/9c892847-2acf-4f34-aaa9-aca2e8ce5d9b";
+      device = "/dev/disk/by-uuid/a81a7f6d-14e3-45b0-9227-786cf603edb0";
       preLVM = true;
       allowDiscards = true;
     };
@@ -32,6 +27,6 @@
   networking.hostName = "Konata";
 
   # The danger setting; see 'default-configuration.nix' for details.
-  system.stateVersion = "20.03";
+  system.stateVersion = "20.09";
 
 }
